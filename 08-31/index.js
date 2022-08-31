daba0cf1320c9676e2db253e9b59f63bc44b6896
MDYmp <tkrhkskan15@gmail.com>
2022-08-31 12:02:41 +0900

    자료형, 연산자 등 배움

---
+++ 08-31/index.js
@@ -0,0 +1,61 @@
+// console.log(1);
+// console.log(1.1);
+// console.log("1");
+
+var a = 4;
+let b = 5;
+const c = 1;
+
+a = 5;
+b = 3;
+
+// console.log('a :', a);
+// console.log('b :', b);
+// console.log('c :', c);
+
+var aPlusb = a + b;
+
+console.log("a+b=", aPlusb);
+console.log("a-b=", a-b);
+console.log("a*b=", a*b);
+console.log("a/b=", a/b);
+console.log("a%b=", a%b);
+// a = a + 4;
+a += 4;
+console.log("a를 9로 만드는 방법", a);
+a -= 4;
+console.log("a를", a);
+a *= 4;
+console.log("a를2", a);
+a /= 4;
+console.log("a를3", a);
+a %= 4;
+console.log("a를3", a);
+a **= 4;
+console.log("a를4", a);
+
+
+var e = 3;
+var result = e--;
+console.log('e :', e);
+console.log('result :', result);
+
+
+var f = true;
+var g = false;
+console.log('f :', f);
+console.log('g :', g);
+
+
+var aa = 3;
+var bb = 3;
+
+console.log('aa < bb :', aa < bb);
+console.log('aa <= bb :', aa <= bb);
+console.log('aa > bb :', aa > bb);
+console.log('aa >= bb :', aa >= bb);
+console.log('aa == bb :', aa == bb);
+console.log('aa != bb :', aa != bb);
+
+console.log('aa === bb :', aa === bb);
+console.log('aa !== bb :', aa !== bb);
